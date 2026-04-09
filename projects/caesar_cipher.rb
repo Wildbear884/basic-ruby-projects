70bdb49ca48a7e027e0072c9c7acdbf12208a678
Wildbear <209266888+Wildbear884@users.noreply.github.com>
2026-04-09 16:26:50 -0400

    Add starting question loop

---
+++ projects/caesar_cipher.rb
@@ -0,0 +1,10 @@
+loop do
+  puts "Would you like to preform a right shift Caeser cipher? (yes or no)"
+  answer = gets.chomp.downcase
+  if answer == "yes"
+    # more code to go here
+  elsif answer == "no"
+    puts "Okay thank you for using this program and have a good day."
+    break
+  end
+end
