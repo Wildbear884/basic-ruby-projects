4c6e9f247a7457861ee16c02b320db3851cb2b72
Wildbear <209266888+Wildbear884@users.noreply.github.com>
2026-04-09 16:36:25 -0400

    Add retrieval of user input for the cipher

--- projects/caesar_cipher.rb
+++ projects/caesar_cipher.rb
@@ -2,7 +2,17 @@ loop do
   puts "Would you like to preform a right shift Caeser cipher? (yes or no)"
   answer = gets.chomp.downcase
   if answer == "yes"
-    # more code to go here
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    shift = 0
+    
+    puts "Please enter what you would like to encode: "
+    starting_string = gets.chomp
+
+    until shift.between?(1, 25)
+      puts "Please enter a number between 1 and 25 for the shift: "
+      shift = gets.chomp.to_i
+    end
+
   elsif answer == "no"
     puts "Okay thank you for using this program and have a good day."
     break
