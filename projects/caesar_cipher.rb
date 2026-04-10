778a91b10b0b42dc8f505ff95ee5569448fc5a6f
Wildbear <209266888+Wildbear884@users.noreply.github.com>
2026-04-10 11:48:12 -0400

    Add code for the encoding the string
    So that the entered string is actually ciphered.

--- projects/caesar_cipher.rb
+++ projects/caesar_cipher.rb
@@ -13,6 +13,20 @@ loop do
       shift = gets.chomp.to_i
     end
 
+    encoded_array = starting_string.chars.map do |char|
+      letter = char.downcase
+
+      if alphabet.include?(letter)
+        shifted_letter_index = alphabet.index(letter) + shift
+        shifted_letter_index -= 26 if shifted_letter_index > 25
+        shifted_letter = alphabet[shifted_letter_index]
+        char = letter != char ? shifted_letter.upcase : shifted_letter
+      end
+
+      char
+    end
+    
+    puts encoded_array.join
   elsif answer == "no"
     puts "Okay thank you for using this program and have a good day."
     break
