3a42c02c28123babc1166ace160f173d23a29790
Wildbear <209266888+Wildbear884@users.noreply.github.com>
2026-04-14 16:37:12 -0400

    Change normal heredoc to squiggly heredoc
    For better code readability and to match the style guide.

--- projects/substrings.rb
+++ projects/substrings.rb
@@ -10,24 +10,24 @@ dictionary =
 
 small_string = "Howdy partner, sit down! How's it going?"
 
-big_string = <<TEXT
-"Howdy partner, sit down! How's it going with you? All's well with you? Good
-good. Now do you want to hear the story of the golden horn? You do? Great!
-The story starts with Billy the cowboy and his friend John. One day they
-went down below into a cave that they had heard a rumor about... ... ... ...
-... Cave-in! ... ... snakes ... spiders ... And when they made it back to town
-after they found the golden horn they sold it to a museum for a considerable
-amount of money that they split fifty-fifty. After that both of them settled
-down, Billy as a rancher and John as a blacksmith."
+big_string = <<~TEXT
+  "Howdy partner, sit down! How's it going with you? All's well with you? Good
+  good. Now do you want to hear the story of the golden horn? You do? Great!
+  The story starts with Billy the cowboy and his friend John. One day they
+  went down below into a cave that they had heard a rumor about... ... ... ...
+  ... Cave-in! ... ... snakes ... spiders ... And when they made it back to town
+  after they found the golden horn they sold it to a museum for a considerable
+  amount of money that they split fifty-fifty. After that both of them settled
+  down, Billy as a rancher and John as a blacksmith."
 TEXT
 
-puts <<OUTPUT_TEXT
-Substrings in:
-"#{small_string}"
+puts <<~OUTPUT_TEXT
+  Substrings in:
+  "#{small_string}"
 
-#{substrings(small_string, dictionary)}
+  #{substrings(small_string, dictionary)}
 
-Substrings in:
-#{big_string}
-#{substrings(big_string, dictionary)}
+  Substrings in:
+  #{big_string}
+  #{substrings(big_string, dictionary)}
 OUTPUT_TEXT
