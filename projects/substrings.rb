b3166539b71bdf83970be1aae5c9d7dfb4e3cb0c
Wildbear <209266888+Wildbear884@users.noreply.github.com>
2026-04-14 15:54:55 -0400

    Rework the output
    So that there are less puts statements, the code is IMO easier to read
    and the output to the terminal looks better.

--- projects/substrings.rb
+++ projects/substrings.rb
@@ -8,4 +8,26 @@ end
 dictionary = 
   %w[below down go going horn how howdy it i low own part partner sit]
 
-puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+small_string = "Howdy partner, sit down! How's it going?"
+
+big_string = <<TEXT
+"Howdy partner, sit down! How's it going with you? All's well with you? Good
+good. Now do you want to hear the story of the golden horn? You do? Great!
+The story starts with Billy the cowboy and his friend John. One day they
+went down below into a cave that they had heard a rumor about... ... ... ...
+... Cave-in! ... ... snakes ... spiders ... And when they made it back to town
+after they found the golden horn they sold it to a museum for a considerable
+amount of money that they split fifty-fifty. After that both of them settled
+down, Billy as a rancher and John as a blacksmith."
+TEXT
+
+puts <<OUTPUT_TEXT
+Substrings in:
+"#{small_string}"
+
+#{substrings(small_string, dictionary)}
+
+Substrings in:
+#{big_string}
+#{substrings(big_string, dictionary)}
+OUTPUT_TEXT
