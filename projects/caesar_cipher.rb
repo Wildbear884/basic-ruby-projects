437ccb18a6fe4ac7113e8a4ea5d753729fa75af6
Wildbear <209266888+Wildbear884@users.noreply.github.com>
2026-04-14 16:46:31 -0400

    Turn the cipher and the user inputs into methods
    So that the code can preform like in the project page on the Odin
    project for it. And the user inputs got moved for code tidiness.

--- projects/caesar_cipher.rb
+++ projects/caesar_cipher.rb
@@ -1,30 +1,39 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-shift = 0
+def caesar_cipher(str, shift)
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-puts <<~WELCOME
-  Welcome! This is a right shift Caesar cipher program.
-  And FYI this program only changes letters in the English alphabet.
-  Please enter what you would like to encode:
-WELCOME
+  str.chars.map do |char|
+    letter = char.downcase
 
-starting_string = gets.chomp
-
-until shift.between?(1, 25)
-  puts "Please enter a number between 1 and 25 for the shift: "
-  shift = gets.chomp.to_i
+    if alphabet.include?(letter)
+      shifted_letter_index = alphabet.index(letter) + shift
+      shifted_letter_index -= 26 if shifted_letter_index > 25
+      shifted_letter = alphabet[shifted_letter_index]
+      char = letter != char ? shifted_letter.upcase : shifted_letter
+    end
+    
+    char
+  end.join
 end
 
-encoded_array = starting_string.chars.map do |char|
-  letter = char.downcase
+def get_shift
+  shift = 0
 
-  if alphabet.include?(letter)
-    shifted_letter_index = alphabet.index(letter) + shift
-    shifted_letter_index -= 26 if shifted_letter_index > 25
-    shifted_letter = alphabet[shifted_letter_index]
-    char = letter != char ? shifted_letter.upcase : shifted_letter
+  until shift.between?(1, 25)
+    puts "Please enter a number between 1 and 25 for the shift:"
+    shift = gets.chomp.to_i
   end
-  
-  char
+
+  shift
 end
 
-puts encoded_array.join
+def get_sentence
+  puts <<~WELCOME
+    Welcome! This is a right shift Caesar cipher program.
+    And FYI this program only changes letters in the English alphabet.
+    Please enter what you would like to encode:
+  WELCOME
+
+  gets.chomp
+end
+
+puts caesar_cipher(get_sentence, get_shift)
