6123b65b2dc7846fbd815251e8c643bd621d7f1e
Wildbear <209266888+Wildbear884@users.noreply.github.com>
2026-04-30 14:40:58 -0400

    Refactor variables
    Because there were some ones that didn't add anything of use to the code
    and there was one that needed renaming to be more descriptive IMO.

--- projects/bubble_sort.rb
+++ projects/bubble_sort.rb
@@ -1,24 +1,21 @@
 def bubble_sort(array)
   arr = array.dup
-  remaining_loops = arr.length - 1
+  iterations_remaining = arr.length - 1
 
   (arr.length - 1).times do
     arr_sorted = true
     
-      remaining_loops.times do |ele|
-      first_number = ele
-      second_number = ele + 1
-
-      if arr[first_number] > arr[second_number]
-        temp = arr[first_number]
-        arr[first_number] = arr[second_number]
-        arr[second_number] = temp
+    iterations_remaining.times do |ele|
+      if arr[ele] > arr[ele + 1]
+        temp_value = arr[ele]
+        arr[ele] = arr[ele + 1]
+        arr[ele + 1] = temp_value
 
         arr_sorted = false
       end
     end
 
-    remaining_loops -= 1
+    iterations_remaining -= 1
     break if arr_sorted
   end
   
